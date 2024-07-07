0bb32ff48ecbe4cb8719716d77609ef2a1fe9e9d
bhattarainabin64 <edrinabidari@gmail.com>
2024-07-07 08:43:22 +0545

    First commit haha

--- lib/profile.dart
+++ lib/profile.dart
@@ -11,7 +11,7 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Profile Page'),
+      child: Text('Profile'),
     );
   }
 }
